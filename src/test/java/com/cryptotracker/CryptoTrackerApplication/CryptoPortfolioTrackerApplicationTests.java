f8723f2de48d45aee38b1640b03f274f032ac2c6
Surya Vardhan <suryavardhankarella@gmail.com>
2025-05-26 12:13:33 +0530

    [Surya] - Added: Project Structure

---
+++ src/test/java/com/cryptotracker/CryptoTrackerApplication/CryptoPortfolioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptotracker.CryptoTrackerApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoPortfolioTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
