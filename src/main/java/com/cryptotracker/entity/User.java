7e66d90cd4d25e9532ef554c3eec2651f0a037ab
SwethaJayasankar <swethajayasankar12@gmail.com>
2025-05-27 11:14:56 +0530

    [Swetha]-Added:User Entity and enum Role

--- src/main/java/com/cryptotracker/entity/User.java
+++ src/main/java/com/cryptotracker/entity/User.java
@@ -1,5 +1,31 @@
 package com.cryptotracker.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long user_id;
+	private String name;
+	private String email;
+	private String password;
+	@Enumerated(EnumType.STRING)
+	private Role role;
 
 }
+
+
+//I have created User class with the attributes and enum role which has user and Admin.
