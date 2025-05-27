c896a3e16e0b490fdd041967bceaa527962b1bd1
SwethaJayasankar <swethajayasankar12@gmail.com>
2025-05-27 15:51:17 +0530

    [Swetha]-Added:UserDTO,UserMapper,UserRepository

--- src/main/java/com/cryptotracker/dto/UserRequestDTO.java
+++ src/main/java/com/cryptotracker/dto/UserRequestDTO.java
@@ -1,5 +1,34 @@
 package com.cryptotracker.dto;
 
-public class UserRequestDTO {
+import com.cryptotracker.entity.Role;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserRequestDTO {
+	
+	
+	@NotBlank(message="Name is Mandatory")
+	@Pattern(regexp = "^[a-zA-Z0-9_]{3,20}$", message = "Name must be 3-20 characters long and contain only letters, numbers, or underscores")
+    private String name;
+	
+	
+	@NotBlank(message = "Email is Mandatory")
+	@Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$",message = "Email should be valid")
+	private String email;
+	
+	 @NotBlank(message = "Password is Mandatory")
+	 @Size(min = 6, message = "Password should be at least 6 characters")
+	 private String password;
+	 
+	 @NotBlank(message = "Role is Mandatory")
+	 private Role role; //role assigned to the user
+	
 }
--- src/main/java/com/cryptotracker/entity/User.java
+++ src/main/java/com/cryptotracker/entity/User.java
@@ -1,11 +1,13 @@
 package com.cryptotracker.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,18 +16,28 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+@Table(name="User")
+public class User //created User class with the attributes and enum role which has user and Admin.
+{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long user_id;
+	
+	@Column(nullable=false)
 	private String name;
+	
+    @Column(nullable=false)
 	private String email;
+    
+	@Column(nullable=false)
 	private String password;
+	
 	@Enumerated(EnumType.STRING)
+	@Column(nullable=false)
 	private Role role;
 
 }
 
 
-//I have created User class with the attributes and enum role which has user and Admin.
+
